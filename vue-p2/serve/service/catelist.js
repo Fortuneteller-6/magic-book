d216fda1b3006b2fd1eb8f26b4e133a523c220aa
bin1 <2639696181@qq.com>
2020-05-23 10:17:24 +0800

    vue

---
+++ vue-p2/serve/service/catelist.js
@@ -0,0 +1,60 @@
+function getCateList() {
+    return [{
+            id: 1,
+            cate_name: 'Modern Chair',
+            cate_price: 180,
+            cate_img: 'http://localhost:3000/bg-img/01.jpg',
+
+        },
+        {
+            id: 2,
+            cate_name: 'Modern Chair',
+            cate_price: 180,
+            cate_img: 'http://localhost:3000/bg-img/02.jpg',
+        },
+        {
+            id: 3,
+            cate_name: 'Modern Chair',
+            cate_price: 180,
+            cate_img: 'http://localhost:3000/bg-img/03.jpg',
+        },
+        {
+            id: 4,
+            cate_name: 'Modern Chair',
+            cate_price: 180,
+            cate_img: 'http://localhost:3000/bg-img/04.jpg',
+        },
+        {
+            id: 5,
+            cate_name: 'Modern Chair',
+            cate_price: 180,
+            cate_img: 'http://localhost:3000/bg-img/05.jpg',
+        },
+        {
+            id: 6,
+            cate_name: 'Modern Chair',
+            cate_price: 180,
+            cate_img: 'http://localhost:3000/bg-img/06.jpg',
+        },
+        {
+            id: 7,
+            cate_name: 'Modern Chair',
+            cate_price: 180,
+            cate_img: 'http://localhost:3000/bg-img/07.jpg',
+        },
+        {
+            id: 8,
+            cate_name: 'Modern Chair',
+            cate_price: 180,
+            cate_img: 'http://localhost:3000/bg-img/08.jpg',
+        },
+        {
+            id: 9,
+            cate_name: 'Modern Chair',
+            cate_price: 180,
+            cate_img: 'http://localhost:3000/bg-img/09.jpg',
+        },
+    ]
+}
+
+module.exports = getCateList
---
+++ vue-p2/新建文件夹/addressdata.js
@@ -0,0 +1,36 @@
+function getCateList(){
+  return [
+    {
+      'name': 'Mr Wang',
+      'phone': '13811111111',
+      'areaCode': '010',
+      'landLine': '64627856',
+      'provinceId': 110000,
+      'province': 'Guang Dong ',
+      'cityId': 110100,
+      'city': 'Shenzhen',
+      'countyId': 110106,
+      'county': 'Futian',
+      'add': 'Room 319, building 6, Shangdi 10th Stree',
+      'default': true,
+      'checked': true
+    },{
+      'name': 'Mr Li',
+      'phone': '13811111111',
+      'areaCode': '010',
+      'landLine': '64627856',
+      'provinceId': 110000,
+      'province': 'Guang Dong ',
+      'cityId': 110100,
+      'city': 'Shenzhen',
+      'countyId': 110106,
+      'county': 'Futian',
+      'add': 'Room 319, building 6, Shangdi 10th Stree',
+      'default': true,
+      'checked': true
+    }
+  ]
+}
+
+module.exports = getCateList  
+
